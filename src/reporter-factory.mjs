c42568f6cb7200d2161c6f00349b3f19df3a0e1d
Marian Bolojan <office.adorade@gmail.com>
2024-06-20 17:26:01 +0300

    chore(release): update to release v2.1.0

--- src/reporter-factory.mjs
+++ src/reporter-factory.mjs
@@ -1,5 +1,5 @@
 /*!
- * Gulp Stylelint (v2.0.0): src/reporter-factory.mjs
+ * Gulp Stylelint (v2.1.0): src/reporter-factory.mjs
  * Copyright (c) 2023-24 Adorade (https://github.com/adorade/gulp-stylelint-esm)
  * License under MIT
  * ========================================================================== */
