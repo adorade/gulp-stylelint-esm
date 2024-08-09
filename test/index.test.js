3ab28ed08449661836bdc4e029eaeb00fa47f35d
Marian Bolojan <office.adorade@gmail.com>
2024-08-09 03:32:04 +0300

    test(option): add test for `failAfterError` option

--- test/index.test.js
+++ test/index.test.js
@@ -86,6 +86,8 @@ describe('Plugin Functionality', () => {
   test('should emit an error when linter complains', async () => {
     const stream = gulp.src(fixtures('invalid.css'));
 
+    expect.assertions(1);
+
     try {
       await new Promise((resolve, reject) => {
         stream
@@ -148,5 +150,180 @@ describe('Plugin Functionality', () => {
       fs.unlinkSync(outputFilePath);
       fs.rmdirSync(outputDir);
     }
+  });
+  test('should not fail after emitting no errors if `failAfterError` is not configured', async () => {
+    const stream = gulp.src(fixtures('basic.css'));
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          config: { rules: {} }
+        }))
+        .on('error', (error) => {
+          expect(error).toBe(undefined);
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+  });
+  test('should not fail after emitting no errors if `failAfterError` is set to true', async () => {
+    const stream = gulp.src(fixtures('basic.css'));
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          failAfterError: true,
+          config: { rules: {} }
+        }))
+        .on('error', (error) => {
+          expect(error).toBe(undefined);
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+  });
+  test('should not fail after emitting no errors if `failAfterError` is set to false', async () => {
+    const stream = gulp.src(fixtures('basic.css'));
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          failAfterError: false,
+          config: { rules: {} }
+        }))
+        .on('error', (error) => {
+          expect(error).toBe(undefined);
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+  });
+  test('should fail after emitting an error if `failAfterError` is not configured', async () => {
+    const stream = gulp.src(fixtures('invalid.css'));
+    let errorEmitted = false;
+
+    expect.assertions(2);
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          config: { rules: { 'color-hex-length': 'short' } },
+        }))
+        .on('error', (error) => {
+          errorEmitted = true;
+          expect(error.message).toBe('Failed with 1 error');
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+
+    expect(errorEmitted).toBe(true);
+  });
+  test('should fail after emitting errors if `failAfterError` is not configured', async () => {
+    const stream = gulp.src(fixtures('invalids.css'));
+    let errorEmitted = false;
+
+    expect.assertions(2);
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          config: { rules: { 'color-hex-length': 'short' } },
+        }))
+        .on('error', (error) => {
+          errorEmitted = true;
+          expect(error.message).toBe('Failed with 2 errors');
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+
+    expect(errorEmitted).toBe(true);
+  });
+  test('should fail after emitting an error if `failAfterError` is set to true', async () => {
+    const stream = gulp.src(fixtures('invalid.css'));
+    let errorEmitted = false;
+
+    expect.assertions(2);
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          failAfterError: true,
+          config: { rules: { 'color-hex-length': 'short' } },
+        }))
+        .on('error', (error) => {
+          errorEmitted = true;
+          expect(error.message).toBe('Failed with 1 error');
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+
+    expect(errorEmitted).toBe(true);
+  });
+  test('should fail after emitting errors if `failAfterError` is set to true', async () => {
+    const stream = gulp.src(fixtures('invalids.css'));
+    let errorEmitted = false;
+
+    expect.assertions(2);
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          failAfterError: true,
+          config: { rules: { 'color-hex-length': 'short' } },
+        }))
+        .on('error', (error) => {
+          errorEmitted = true;
+          expect(error.message).toBe('Failed with 2 errors');
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+
+    expect(errorEmitted).toBe(true);
+  });
+  test('should not fail after emitting an error if `failAfterError` is set to false', async () => {
+    const stream = gulp.src(fixtures('invalid.css'));
+    let errorEmitted = false;
+
+    expect.assertions(1);
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          failAfterError: false,
+          config: { rules: { 'color-hex-length': 'short' } },
+        }))
+        .on('error', () => {
+          errorEmitted = true;
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+
+    expect(errorEmitted).toBe(false);
+  });
+  test('should not fail after emitting errors if `failAfterError` is set to false', async () => {
+    const stream = gulp.src(fixtures('invalids.css'));
+    let errorEmitted = false;
+
+    expect.assertions(1);
+
+    await new Promise((resolve) => {
+      stream
+        .pipe(gStylelintEsm({
+          failAfterError: false,
+          config: { rules: { 'color-hex-length': 'short' } },
+        }))
+        .on('error', () => {
+          errorEmitted = true;
+          resolve;
+        })
+        .on('finish', resolve);
+    });
+
+    expect(errorEmitted).toBe(false);
   });
 });
